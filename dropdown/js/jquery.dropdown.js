8cab8fbca9446ed670fcc19630cc8e5954ca06a9
Bryant Rolfe <bryant.rolfe@gmail.com>
2012-03-07 20:00:35 -0500

    Basic setup complete. About to begin development!

---
+++ dropdown/js/jquery.dropdown.js
@@ -0,0 +1,5 @@
+(function($){
+  $.fn.dropdown = function() {
+    // plugin code
+  };
+})(jQuery);
---
+++ dropdown/js/test_dropdown.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+  // Do my business
+  // Test it.
+  $('#dropdown_div').text("Hello World");
+});
