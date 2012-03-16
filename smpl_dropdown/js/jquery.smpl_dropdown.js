3f2fc64e63554e9e53d35f2ed1611da277bca593
Bryant Rolfe <bryant.rolfe@gmail.com>
2012-03-15 22:52:09 -0400

    Adding smpl_drop object with various methods and unit tests.

--- smpl_dropdown/js/jquery.smpl_dropdown.js
+++ smpl_dropdown/js/jquery.smpl_dropdown.js
@@ -1,15 +1,38 @@
 (function($){
-  "use strict";
-  $.fn.smpl_dropdown = function(options) {
+"use strict";
 
-    var defaults = {
-      'names'     : ['name1', 'name2'],
-      'values'    : ['value1', 'value2'],
-      'default'   : null
+$.fn.smpl_dropdown = function(options) {
+    // Check to see if this element already has an smpl_drop instance
+    var smpl_drop = this.get_smpl_drop();
+    if( smpl_drop ) {
+        // This element already has a smpl_drop
+        return smpl_drop;
     }
 
-    var settings = $.extend(true, defaults, options);
-    
+    // Create a smpl_drop instance and put it in element storage
+    // TODO smpl_drop uses no options at this point...
+    smpl_drop = new $.smpl_drop({}, this[0]);
+    $.data(this[0], 'smpl_drop', smpl_drop);
+
+    smpl_drop.set(options);
+
+    return smpl_drop;
+};
+
+$.fn.get_smpl_drop = function() {
+    return $.data(this[0], 'smpl_drop');
+}
+
+// Constructor
+$.smpl_drop = function(options, element) {
+    this.settings = $.extend( true, {}, $.smpl_drop.defaults, options );
+    this.element = element;
+    this.init();
+};
+
+$.smpl_drop.defaults =  {};
+
+$.smpl_drop.validateSettings = function(settings) {
     // Input validation
     if(settings.names.length !== settings.values.length) {
       throw "array args to smpl_dropdown must be the same length.";
@@ -17,25 +40,87 @@
     if(settings.default && $.inArray(settings.default, settings.values) === -1) {
       throw 'Default value ' + settings.default + ' is not in ' + settings.values;
     }
+};
 
-    // Create the dropdown
-    var select = $('<select>', {
-      'class' : 'smpl_dropdown'
-    });
+$.smpl_drop.prototype =  {
+    init: function() {
+        //TODO 
+    },
 
-    $.each(settings.names, function(index, name_text){
-      select.append($('<option>', {
-        'text'    : name_text,
-        'value'   : settings.values[index]
-      }));
-    });
-  
-    if(settings.default) {
-      select.val(settings.default);
+    // Public
+    getValue: function() {
+        return $(this.select).val();
+    },
+
+    // Public
+    getNames: function() {
+        var names = [];
+        var options = this.getOptionElements();
+        $.each(options, function(index, opt){
+            names.push($(opt).text());
+        });
+        return names;
+    },
+
+    // Public
+    getValues: function() {
+        var values = [];
+        var options = this.getOptionElements();
+        $.each(options, function(index, opt){
+            values.push($(opt).val());
+        });
+        return values;
+    },
+
+    set: function(options) {
+
+        // Clear old smpl dropdowns
+        if(this.select) {
+            this.select.detach();
+        }
+
+        var defaults = {
+          'names'     : ['name1', 'name2'],
+          'values'    : ['value1', 'value2'],
+          'default'   : null
+        }
+
+        var settings = $.extend(true, defaults, options);
+       
+        $.smpl_drop.validateSettings(settings);
+
+        // Create the dropdown
+        var select = $('<select>', {
+          'class' : 'smpl_dropdown'
+        });
+
+        $.each(settings.names, function(index, name_text){
+          select.append($('<option>', {
+            'text'    : name_text,
+            'value'   : settings.values[index]
+          }));
+        });
+      
+        if(settings.default) {
+          select.val(settings.default);
+        }
+
+        $(this.element).append(select);
+        this.select = select;
+    },
+
+    // Public
+    setValue: function(new_val) {
+        if($.inArray(new_val, this.getValues()) > -1) {
+            this.select.val(new_val);
+        }else {
+            throw "Cannot set to that val.";
+        }
+    },
+
+    getOptionElements: function() {
+        return $(this.select).find('option');
     }
+}
 
-    this.append(select);
-
-    return this;
-  };
 })(jQuery);
--- smpl_dropdown/js/test_smpl_dropdown.js
+++ smpl_dropdown/js/test_smpl_dropdown.js
@@ -1,12 +1,52 @@
 $(document).ready(function(){
-  "use strict";
-  // Do my business
-  // Test it.
-  var info =  {
-    names   : ['A', 'B', 'C'],
-    values  : ['a', 'b', 'c'],
-    default : 'b'
-  }
+    "use strict";
+    // Do my business
+    // Test it.
+    var info =  {
+        names   : ['A', 'B', 'C'],
+        values  : ['a', 'b', 'c'],
+        default : 'b'
+    };
+    
+    var info2 =  {
+        names   : ['D', 'E', 'F'],
+        values  : ['d', 'e', 'f']
+    };
 
-  $('#dropdown_div').smpl_dropdown(info);
+    var drop = $('#dropdown_div').smpl_dropdown(info);
+
+    test_dropdown(drop, info, 1);
+
+    drop.setValue('a');
+    info.default = 'a';
+    test_dropdown(drop, info, 5);
+
+    drop.set(info2);
+    info2.default = 'd';
+    test_dropdown(drop, info2, 9);
+
 });
+
+var test_dropdown = function(drop, answers, start_num) {
+    
+    var test_num = start_num;
+
+    testEqual(test_num++, "getValue()", answers.default, drop.getValue());
+    
+    testEqual(test_num++, "getNames()", answers.names, drop.getNames());
+    
+    testEqual(test_num++, "getValues()", answers.values, drop.getValues());
+    
+    testEqual(test_num++, "getValues()", answers.values, drop.getValues());
+}
+
+var testEqual = function(test_num, test_desc, reference, given) {
+    var text = "Test " + test_num + " - " + test_desc;
+    if(JSON.stringify(given) === JSON.stringify(reference)) {
+        console.log(text + " passed.");
+        return true;
+    }else {
+        console.warn(text + " failed. Expected, Given", reference, given);
+        return false;
+    }
+}
